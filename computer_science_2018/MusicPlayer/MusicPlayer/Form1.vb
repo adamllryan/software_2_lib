2f5a25de54a87f583d41deec4f681caa126310d9
Adam <adamllryan@gmail.com>
2023-08-05 11:31:18 -0400

    chunk 12

---
+++ computer_science_2018/MusicPlayer/MusicPlayer/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class SmashPlayer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Audio.Play("S:\SmashBrawl.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Computer.Audio.Play("S:\SmashBrawlLoud.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.Audio.Play("S:\Smash64.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        My.Computer.Audio.Play("S:\SmashMelee.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        My.Computer.Audio.Play("S:\Smash4.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        My.Computer.Audio.Play("S:\SmashUltimate.wav",
+   AudioPlayMode.BackgroundLoop)
+    End Sub
+End Class
